c7bb323b200b8c34a62f41866c6998dca7b77bb6
Pabanthapa91 <aanayabthapa@gmail.com>
2020-08-01 11:31:17 -0400

    Git_Day_01  check class FYI

---
+++ src/Git_Day_01.java
@@ -0,0 +1,13 @@
+public class Git_Day_01 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Happy Asia");
+        System.out.println("How are you ?");
+
+
+
+
+
+    }
+}
