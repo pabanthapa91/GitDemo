c8c74ab70ca5fe041582b3bfed4c8a9e17a44c8e
Pabanthapa91 <aanayabthapa@gmail.com>
2020-08-01 12:08:15 -0400

    Gitpractice.java

---
+++ src/Gitpractice.java
@@ -0,0 +1,15 @@
+public class Gitpractice {
+    public static void main(String[] args) {
+
+
+       int a = 1;
+       int b = 2;
+       if(a>b){
+           System.out.println("happy firday");
+       }else{
+           System.out.println("do more cooding");
+
+
+       }
+    }
+}
