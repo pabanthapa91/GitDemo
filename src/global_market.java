78c8adc758499cd138ae0a14171af5139f48c340
Pabanthapa91 <aanayabthapa@gmail.com>
2020-08-03 14:15:11 -0400

    added new class paban.java

--- src/global_market.java
+++
@@ -1,9 +0,0 @@
-public class global_market {
-    public static void main(String[] args) {
-
-
-        System.out.println("hello");
-        
-        // my name is paban
-    }
-}
---
+++ src/paban.java
@@ -0,0 +1,7 @@
+public class paban {
+    public static void main(String[] args) {
+
+
+        System.out.println("git practice ");
+    }
+}
