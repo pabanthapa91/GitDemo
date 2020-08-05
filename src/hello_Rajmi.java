c7b2ecad76cb6c9c106b602ebc96b7a6a2d9341f
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-04 21:19:22 -0400

    Update hello_Rajmi.java

--- src/hello_Rajmi.java
+++ src/hello_Rajmi.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class hello_Rajmi {
     public static void main(String[] args) {
         System.out.println(" K cha , Khana khaio ?");
+        // khaye ani timi lay khayou ta?
     }
 }
